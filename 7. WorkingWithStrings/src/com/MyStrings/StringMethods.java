c373cf8b734f61806c35713fe346e1c9e0add648
Vladimir101 <vladimir100@hotmail.com>
2019-07-05 11:50:05 -0700

    indent() and format() examples added

--- 7. WorkingWithStrings/src/com/MyStrings/StringMethods.java
+++ 7. WorkingWithStrings/src/com/MyStrings/StringMethods.java
@@ -12,11 +12,23 @@ public class StringMethods
 		System.out.println("Position of I: " + pos);
 		
 		int length = palindrome.length();
+// indent was introduced in Java 12
+		System.out.print(("Palindrom length: " + length).indent(3));
+		
 		String palindrome2 = "Madam Im Adam";
 		
 		if (palindrome.equals(palindrome2))
 			System.out.println("Strings are equal");
 		else
 			System.out.println("Strings are NOT equal");
+		
+		int intVar = 24;	 
+        float floatVar = 6.6f;
+        char charVar = 'V';
+        String strVar = "Hello, Java 12";
+
+// displaying formatted string
+        System.out.printf("The formatted string: %d %f %c %s", 
+        		intVar , floatVar, charVar, strVar);
 	}
 }
