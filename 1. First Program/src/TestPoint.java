3f729df61c47d30e93cab7709d00431e8731545d
Vladimir101 <vladimir100@hotmail.com>
2019-07-22 14:05:55 -0700

    adding var

--- 1. First Program/src/TestPoint.java
+++ 1. First Program/src/TestPoint.java
@@ -1,15 +1,17 @@
-
 public class TestPoint
 {
 
 	public static void main(String[] args)
 	{
 		Point point = new Point(3, 4);
+
 		double distance = point.distanceFromOrigin();
 		System.out.println("Distance from origin for (" +
 				point.getX() + ", " + point.getY() +
 				") is " + distance);
 
+// Java 10 - type inference for LOCAL variables
+// Variables must be initialized at the time of declaration		
+		var point2 = new Point(24, 66);
 	}
-
 }
