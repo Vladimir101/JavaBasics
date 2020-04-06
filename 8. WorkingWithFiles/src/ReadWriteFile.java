6aa06e6e27b091e66947a7e974a586b86e3c4ee8
Vladimir101 <vladimir100@hotmail.com>
2020-04-06 12:49:51 -0700

    try-with-resources concept

--- 8. WorkingWithFiles/src/ReadWriteFile.java
+++ 8. WorkingWithFiles/src/ReadWriteFile.java
@@ -2,25 +2,34 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-
+// using try-with-resources concept
 public class ReadWriteFile
 {
-	public static void main(String[] args) throws FileNotFoundException
+	public static void main(String[] args)
 	{
 // read from file		
 		File readFile = new File("readfile.txt");
-		Scanner scanFile = new Scanner(readFile);
-		
-		while (scanFile.hasNextLine())
-			System.out.println(scanFile.nextLine());
-		scanFile.close();
+		try (Scanner scanFile = new Scanner(readFile))
+		{
+			while (scanFile.hasNextLine())
+				System.out.println(scanFile.nextLine());
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
 		
 // write into a file
 		File writeFile = new File("writeFile.txt");
-		PrintWriter printFile = new PrintWriter(writeFile);
-		printFile.println("My first string");
-		printFile.println("My first string");
-		printFile.print("My first string");
-		printFile.close();
+		try(PrintWriter printFile = new PrintWriter(writeFile))
+		{
+			printFile.println("My first string");
+			printFile.println("My first string");
+			printFile.print("My first string");
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
