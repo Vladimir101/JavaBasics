fd2a8eda7b8062765b2b4703ad36e0fbdc2e6367
Vladimir101 <vladimir100@hotmail.com>
2020-04-24 13:59:28 -0700

    Records in Java 14

---
+++ 12. POJO/src/Person.java
@@ -0,0 +1,3 @@
+
+public record Person(String firstName, int age)
+{}
---
+++ 12. POJO/src/TestPerson.java
@@ -0,0 +1,10 @@
+
+public class TestPerson
+{
+	public static void main(String[] args)
+	{
+		Person p = new Person("Tony", 24);
+		System.out.println(p.firstName());
+		System.out.println(p.age());
+	}
+}
